fdc6c9a0efb4f686fca5e63715e21c0c7ae2db9f
vamsi marripudi <vmarripudi49@gmail.com>
2026-04-24 23:29:41 +0530

    Fetching properly.EOD Commit.

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -51,7 +51,7 @@ const Forms = () => {
     }   
 
     useEffect(()=> {
-        const url = "https://multi-step-server.onrender.com/formdata";
+        const url = "http://localhost:5000/formdata";
        const  options = {
             method: "POST",
             headers: {
@@ -60,6 +60,15 @@ const Forms = () => {
             body: JSON.stringify(formData)
 
         }
+        fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data sent successfully:", data);
+        })
+        .catch(error => {
+            console.error("Error sending data:", error);
+        })
+        
     }, [formData]);
 
 
