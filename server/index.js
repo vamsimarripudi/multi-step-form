10a571fc0149b795d68912100ab588f16d33db7b
vamsi marripudi <vmarripudi49@gmail.com>
2026-04-26 20:59:13 +0530

    Resolved cors issue.

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://multi-step.vamsimarripudi.tech',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+})));
 
 // -----------------------------
 // DB Connection
