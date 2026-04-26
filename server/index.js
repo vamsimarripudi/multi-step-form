f7953eb15021d94dad8f9d9ec4ae989547709de3
vamsi marripudi <vmarripudi49@gmail.com>
2026-04-26 22:25:35 +0530

    testing deployed one.

--- server/index.js
+++ server/index.js
@@ -10,11 +10,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-  origin: 'https://multi-step.vamsimarripudi.tech',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+
+app.use(cors());
 
 // -----------------------------
 // DB Connection
